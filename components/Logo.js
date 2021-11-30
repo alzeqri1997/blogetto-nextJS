eab0c92eaa26b8ae09a060a457935902af41e6c9
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-30 06:07:47 +0300

    warp Logo Component with Link Component

--- components/Logo.js
+++ components/Logo.js
@@ -1,10 +1,13 @@
-import { Typography } from "@mui/material";
-import style from "../styles/logo.module.css";
+import { Typography } from '@mui/material'
+import style from '../styles/logo.module.css'
+import Link from 'next/link'
 
 export default function Logo() {
   return (
-    <Typography className={style.logo} variant='body1'>
-      <span>B</span>loggeto
-    </Typography>
-  );
+    <Link href="/" passHref>
+      <Typography className={style.logo} variant="body1">
+        <span>B</span>loggeto
+      </Typography>
+    </Link>
+  )
 }
