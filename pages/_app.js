82e9169665326468e404d7f911f7c5715dd32c99
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-30 06:05:42 +0300

    include custom Theme inside Header,Component,Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { CustomTheme } from '../components/CustomTheme'
+import CssBaseline from '@mui/material/CssBaseline'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { Container } from '@mui/material'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CustomTheme>
+      <CssBaseline />
+      <Header />
+      <Container maxWidth="md" component="main">
+        <Component {...pageProps} />
+      </Container>
+      <Footer />
+    </CustomTheme>
+  )
 }
 
 export default MyApp
