ec5560ecf078b3b1c9816240d9e89c2d7ce45e98
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-30 05:59:17 +0300

    add newsLetterForm Component

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -1,3 +1,24 @@
+import { Typography, Box, TextField, Button } from '@mui/material'
 import CustomButton from '../components/CustomButton'
 
+export default function NewsletterForm() {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ marginBottom: '1rem' }}>
+        <Typography variant="h6" gutterBottom>
+          Subscribe to the newsletter
+        </Typography>
+        <Box alignItems="center" component="form" noValidate autoComplete="off">
+          <TextField
+            sx={{ mr: '1rem' }}
+            id="outlined-basic"
+            label="Enter your Email"
+            variant="outlined"
+            size="small"
+          />
           <CustomButton variant="contained">sign up</CustomButton>
+        </Box>
+      </div>
+    </Box>
+  )
+}
