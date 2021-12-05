91c785c4f57e73bb435442388a7f09e13508b50b
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-05 21:45:39 +0300

    refactor Code

--- components/Title.js
+++ components/Title.js
@@ -1,25 +1,36 @@
-import { Typography } from '@mui/material'
+import { Typography, Box, TextField } from '@mui/material'
+import { GitHub, Github } from './Icons'
 
-export default function Title() {
+export default function Title({ isBlog = false }) {
   return (
     <>
       <Typography
-        sx={{ fontWeight: 'bold' }}
+        sx={{ fontWeight: 'bold', mb: '0' }}
         variant="h3"
         component="h1"
         gutterBottom
       >
-        Sticky footer
+        Latest
       </Typography>
-      <Typography
-        sx={{ fontWeight: 'lighter', fontSize: '1.2rem' }}
-        variant="h5"
-        component="p"
-        gutterBottom
-      >
-        {'Pin a footer to the bottom of the viewport.'}
-        {'The footer will move as the main element of the page grows.'}
-      </Typography>
+      {!isBlog && (
+        <Typography
+          sx={{ fontWeight: 'lighter', fontSize: '1.2rem' }}
+          variant="h5"
+          component="p"
+          gutterBottom
+        >
+          A blog created with Next.js and Tailwind.css
+        </Typography>
+      )}
+
+      {isBlog && (
+        <TextField
+          style={{ width: '50%', marginTop: '1rem' }}
+          id="input-with-sx"
+          label="Search Articles"
+          variant="outlined"
+        />
+      )}
     </>
   )
 }
