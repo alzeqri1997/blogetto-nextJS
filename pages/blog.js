7d76289ba5933e4c9327ef45fb898df0389c3275
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-05 21:46:15 +0300

    add blog page

---
+++ pages/blog.js
@@ -0,0 +1,18 @@
+import Title from '../components/Title'
+import Post from '../components/Posts'
+
+export default function Blog() {
+  return (
+    <>
+      <Title isBlog={true} />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  )
+}
